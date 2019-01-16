4dab94a3db74c5c9a402681d12083417df36ddea
oashvgr <oashadrina92@gmail.com>
2019-01-16 17:31:20 +0300

    first lesson

--- core/src/org/ldvgr/game/Shooter.java
+++ core/src/org/ldvgr/game/Shooter.java
@@ -13,12 +13,12 @@ public class Shooter extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("texture.jpg");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0.2f, 0.7f, 0.6f,0.8f);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
